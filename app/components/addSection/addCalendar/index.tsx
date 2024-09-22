48c5ff4ab0e741b2dd4341caceba4dd169c1a0fa
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-09-22 01:24:21 +0100

    Update index.tsx

--- app/components/addSection/addCalendar/index.tsx
+++ app/components/addSection/addCalendar/index.tsx
@@ -4,7 +4,7 @@ import CalendarInputs from "../../miniComponents/caledarInputs"
 import Button from "../../atoms/button"
 import axios from "axios"
 import { API_BASE_URL } from "@/config"
-import './CustomScrollbar.css';
+import './customScrollbar.css';
 
 interface CalendarBlock {
     selectedNumber: number,
