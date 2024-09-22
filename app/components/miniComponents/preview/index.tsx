8d47b382bac4c2168b839034e915ba60467dab92
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-09-22 01:31:31 +0100

    Update index.tsx

--- app/components/miniComponents/preview/index.tsx
+++ app/components/miniComponents/preview/index.tsx
@@ -8,16 +8,16 @@ interface Props {
   content: string;
   src:string;
   onClick: () => void;
-  children: string;
+  prevText: string;
 }
 
-const Preview = ({ title, content, src , onClick,children }: Props) => {
+const Preview = ({ title, content, src , onClick,prevText }: Props) => {
   return (
     <div>
       <h1 className="text-bold capitalize mb-2">{title}</h1>
       <Image src={src} alt="news image" width={100} height={100} className="mb-2"/>
       <p className="mb-2 text-sm sm:text-auto first-letter:capitalize "><span className="p-2"></span>{content}</p>
-      <Button href="#" children={children} onClick={onClick} />
+      <Button href="#" text={prevText} onClick={onClick} />
     </div>
   );
 };
