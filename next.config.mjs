3b03b63a4be0298444ff16d7fac7dc23d644a85b
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-09-23 14:24:30 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
+        domains: ['ar-rajaa-schools-backend.onrender.com'],
       },
 };
 
